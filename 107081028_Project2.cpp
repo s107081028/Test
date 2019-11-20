407f90d8e6249cef34b222462a2a9829a6bbe0bc
s107081028 <stanleychang0914110366@gmail.com>
2019-11-20 17:41:31 +0800

    Update 107081028_Project2.cpp

--- 107081028_Project2.cpp
+++ 107081028_Project2.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+typedef struct _Node{
+    int r;
+    int c;
+}pos;
 fstream file;
 int row = 0, col = 0, elec = 0, btr = 0, btc = 0, floor = 0, far = 0, farr = 0, farc = 0, bat = 0, now = 0;
 pos tour[100000000] = {0};
@@ -11,27 +15,6 @@ void calpath(int a, int b, int board[][1002], int sorted[][1002], int cur)
     if(a + 1 <= row && board[a + 1][b] == 1 && (sorted[a + 1][b] == 0 || sorted[a + 1][b] > cur + 1)) calpath(a + 1, b, board, sorted, cur + 1);
     if(b - 1 > 0 && board[a][b - 1] == 1 && (sorted[a][b - 1] == 0 || sorted[a][b - 1] > cur + 1)) calpath(a, b - 1, board, sorted, cur + 1);
     if(b + 1 <= col && board[a][b + 1] == 1 && (sorted[a][b + 1] == 0 || sorted[a][b + 1] > cur + 1)) calpath(a, b + 1, board, sorted, cur + 1);
-}
-void sortpath(int board[][1000], int sorted[][1000], pos level[][1000])
-{
-    for(int i = 0; i < row; i++)
-    {
-        for(int j = 0; j < col; j++)
-        {
-            if(board[i][j] == 1 && sorted[i][j] != 0)
-            {
-                level[sorted[i][j]][level[far + 1][sorted[i][j]].r].r = i;
-                level[sorted[i][j]][level[far + 1][sorted[i][j]].r].c = j;
-                level[far + 1][sorted[i][j]].r += 1;
-            }
-        }
-    }
-}
-void fight()
-{
-    bat--;
-    if(bat ==  || floor == 0) home();
-    else if()
 }
 void home(int a, int b, int board[][1002], int sorted[][1002])
 {
@@ -52,33 +35,10 @@ void home(int a, int b, int board[][1002], int sorted[][1002])
 void clean(int a, int b, int board[][1002], int sorted[][1002])
 {
     if(board[a][b] == 1){ board[a][b]++; floor--;}
-    ///else if(board[a][b] < 4 && board[a][b] > 1) board[a][b]++;
-    else if(board[a][b] > 1 && board[a][b] != 10000001) board[a][b]++;
+    else if(board[a][b] < 4 && board[a][b] > 1) board[a][b]++;
     if(bat == sorted[a][b] || floor == 0){ home(a, b, board, sorted); return;}
 
-    int a2 = a - 1, a3 = a + 1, b2 = b - 1, b3 = b + 1;
-    if(a - 1 > 0 && board[a - 1][b] == 1 && sorted[a - 1][b] > sorted[a][b]){tour[now].r = a - 1; tour[now].c = b; now++; bat--; clean(a - 1, b, board, sorted);}
-    else if(a + 1 <= row && board[a + 1][b] == 1 && sorted[a + 1][b] > sorted[a][b]){tour[now].r = a + 1; tour[now].c = b; now++; bat--; clean(a + 1, b, board, sorted);}
-    else if(b - 1 > 0 && board[a][b - 1] == 1 && sorted[a][b - 1] > sorted[a][b]){tour[now].r = a; tour[now].c = b - 1; now++; bat--; clean(a, b - 1, board, sorted);}
-    else if(b + 1 <= col && board[a][b + 1] == 1 && sorted[a][b + 1] > sorted[a][b]){tour[now].r = a; tour[now].c = b + 1; now++; bat--; clean(a, b + 1, board, sorted);}
-    else if(a - 1 > 0 && board[a - 1][b] == 1 && sorted[a - 1][b] != 0){tour[now].r = a - 1; tour[now].c = b; now++; bat--; clean(a - 1, b, board, sorted);}
-    else if(a + 1 <= row && board[a + 1][b] == 1 && sorted[a + 1][b] != 0){tour[now].r = a + 1; tour[now].c = b; now++; bat--; clean(a + 1, b, board, sorted);}
-    else if(b - 1 > 0 && board[a][b - 1] == 1 && sorted[a][b - 1] != 0){tour[now].r = a; tour[now].c = b - 1; now++; bat--; clean(a, b - 1, board, sorted);}
-    else if(b + 1 <= col && board[a][b + 1] == 1 && sorted[a][b + 1] != 0){tour[now].r = a; tour[now].c = b + 1; now++; bat--; clean(a, b + 1, board, sorted);}
-    else if(a2 > 0 && board[a2][b] > 0 && board[a2][b] != 10000000 && board[a2][b] <= board[a3][b] && board[a2][b] <= board[a][b2] && board[a2][b] <= board[a][b3] && sorted[a2][b] > sorted[a][b]){tour[now].r = a - 1; tour[now].c = b; now++; bat--; clean(a - 1, b, board, sorted);}
-    else if(a3 <= row && board[a3][b] > 0 && board[a3][b] != 10000000 && board[a3][b] <= board[a2][b] && board[a3][b] <= board[a][b2] && board[a3][b] <= board[a][b3] && sorted[a3][b] > sorted[a][b]){tour[now].r = a + 1; tour[now].c = b; now++; bat--; clean(a + 1, b, board, sorted);}
-    else if(b2 > 0 && board[a][b2] > 0 && board[a][b2] != 10000000 && board[a][b2] <= board[a2][b] && board[a][b2] <= board[a3][b] && board[a][b2] <= board[a][b3] && sorted[a][b2] > sorted[a][b]){tour[now].r = a; tour[now].c = b - 1; now++; bat--; clean(a, b - 1, board, sorted);}
-    else if(b3 <= col && board[a][b3] > 0 && board[a][b3] != 10000000 && board[a][b3] <= board[a2][b] && board[a][b3] <= board[a3][b] && board[a][b3] <= board[a][b2] && sorted[a][b3] > sorted[a][b]){tour[now].r = a; tour[now].c = b + 1; now++; bat--; clean(a, b + 1, board, sorted);}
-    else if(a2 > 0 && board[a2][b] > 0 && board[a2][b] != 10000000 && board[a2][b] <= board[a3][b] && board[a2][b] <= board[a][b2] && board[a2][b] <= board[a][b3] && sorted[a2][b] != 0){tour[now].r = a - 1; tour[now].c = b; now++; bat--; clean(a - 1, b, board, sorted);}
-    else if(a3 <= row && board[a3][b] > 0 && board[a3][b] != 10000000 && board[a3][b] <= board[a2][b] && board[a3][b] <= board[a][b2] && board[a3][b] <= board[a][b3] && sorted[a3][b] != 0){tour[now].r = a + 1; tour[now].c = b; now++; bat--; clean(a + 1, b, board, sorted);}
-    else if(b2 > 0 && board[a][b2] > 0 && board[a][b2] != 10000000 && board[a][b2] <= board[a2][b] && board[a][b2] <= board[a3][b] && board[a][b2] <= board[a][b3] && sorted[a][b2] != 0){tour[now].r = a; tour[now].c = b - 1; now++; bat--; clean(a, b - 1, board, sorted);}
-    else if(b3 <= col && board[a][b3] > 0 && board[a][b3] != 10000000 && board[a][b3] <= board[a2][b] && board[a][b3] <= board[a3][b] && board[a][b3] <= board[a][b2] && sorted[a][b3] != 0){tour[now].r = a; tour[now].c = b + 1; now++; bat--; clean(a, b + 1, board, sorted);}
-    else if(a - 1 > 0 && board[a - 1][b] == 10000001){tour[now].r = a - 1; tour[now].c = b; now++; return;}
-    else if(a + 1 <= row && board[a + 1][b] == 10000001){tour[now].r = a + 1; tour[now].c = b; now++; return;}
-    else if(b - 1 > 0 && board[a][b - 1] == 10000001){tour[now].r = a; tour[now].c = b - 1; now++; return;}
-    else if(b + 1 <= col && board[a][b + 1] == 10000001){tour[now].r = a; tour[now].c = b + 1; now++; return;}
-
-    /*if(a - 1 >= 0 && board[a - 1][b] == 1 && sorted[a - 1][b] > sorted[a][b]){tour[now].r = a - 1; tour[now].c = b; now++; bat--; clean(a - 1, b, board, sorted);}
+    if(a - 1 >= 0 && board[a - 1][b] == 1 && sorted[a - 1][b] > sorted[a][b]){tour[now].r = a - 1; tour[now].c = b; now++; bat--; clean(a - 1, b, board, sorted);}
     else if(a + 1 < row && board[a + 1][b] == 1 && sorted[a + 1][b] > sorted[a][b]){tour[now].r = a + 1; tour[now].c = b; now++; bat--; clean(a + 1, b, board, sorted);}
     else if(b - 1 >= 0 && board[a][b - 1] == 1 && sorted[a][b - 1] > sorted[a][b]){tour[now].r = a; tour[now].c = b - 1; now++; bat--; clean(a, b - 1, board, sorted);}
     else if(b + 1 < col && board[a][b + 1] == 1 && sorted[a][b + 1] > sorted[a][b]){tour[now].r = a; tour[now].c = b + 1; now++; bat--; clean(a, b + 1, board, sorted);}
@@ -113,7 +73,7 @@ void clean(int a, int b, int board[][1002], int sorted[][1002])
     else if(a - 1 >= 0 && board[a - 1][b] == 0){tour[now].r = a - 1; tour[now].c = b; now++; return;}
     else if(a + 1 < row && board[a + 1][b] == 0){tour[now].r = a + 1; tour[now].c = b; now++; return;}
     else if(b - 1 >= 0 && board[a][b - 1] == 0){tour[now].r = a; tour[now].c = b - 1; now++; return;}
-    else if(b + 1 < col && board[a][b + 1] == 0){tour[now].r = a; tour[now].c = b + 1; now++; return;}*/
+    else if(b + 1 < col && board[a][b + 1] == 0){tour[now].r = a; tour[now].c = b + 1; now++; return;}
 }
 int main(void)
 {
@@ -149,15 +109,15 @@ int main(void)
         }
     }
     calpath(btr, btc, board, sorted, 0);
-    sortpath(board, sorted, level);
+    ///sortpath(board, sorted, level);
     tour[now].r = btr;
     tour[now].c = btc;
     now++;
     while(floor)
     {
+        ///fight();
         bat = elec;
-        fight();
-        home();
+        clean(btr, btc, board, sorted);
     }
     cout<<now<<endl;
     for(int i = 0; i < now; i++)
